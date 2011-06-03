4e70137c414714ce0fdcbe14a8912da06831c9ef
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-06-03 11:48:47 +0700

    Files are uploading correctly now to a test folder that is hardwired - next, the folder id

--- code/Html5UploadController.php
+++ code/Html5UploadController.php
@@ -10,17 +10,142 @@ class Html5UploadController extends Controller {
 		error_log("BASE FOLDER:".Director::baseFolder());
 		error_log("UPLOAIFY UPLOADER FOR HTML5");
 
-		$upload_folder = Director::baseFolder().'/'.ASSETS_DIR.'/flickr';
+		$assets_folder_fs = Director::baseFolder().'/'.ASSETS_DIR.'';
+		$upload_folder = $assets_folder_fs.'/flickr';
+error_log('T1');
+
+		if(isset($_REQUEST['FolderID'])) {
+			error_log("**** FOLDER ID SET ON UPLOAD");
+			if($folder = DataObject::get_by_id("Folder", Convert::raw2sql($_REQUEST['FolderID']))) {
+				$upload_folder = UploadifyField::relative_asset_dir($folder->Filename);
+			}
+		
+		}
+
+error_log('T2');
+
+		$folder = $folder = DataObject::get_by_id('Folder', 208);
+		error_log("post get folder");
+
+		//FIXME - hardwired for testing
+		if($folder) {
+			error_log("Found folder");
+			$upload_folder = UploadifyField::relative_asset_dir($folder->Filename);
+		} 
+
+error_log("T3");	
+	
 
 		error_log("UPLOAD FOLDER:".$upload_folder);
 
 		if(count($_FILES)>0) {
-			error_log("T1");
-        	if( move_uploaded_file( $_FILES['upload']['tmp_name'] , $upload_folder.'/'.$_FILES['upload']['name'] ) ) {
+			error_log("FILES T1");
+			error_log("MOVING FROM:". $_FILES['upload']['tmp_name']);
+			$to_path = $assets_folder_fs.'/'.$upload_folder.$_FILES['upload']['name'];
+			error_log("TO:".$to_path );
+        	if( move_uploaded_file( $_FILES['upload']['tmp_name'] , $to_path ) ) {
                 echo 'done';
-                error_log("T1a");
+                error_log("FILES T1a");
         	}
-        	error_log("T2");
+        	error_log("FILES T2");
+
+
+        	$ext = strtolower(end(explode('.', $_FILES['upload']['name'])));
+        	error_log("EXT:".$ext);
+
+        	error_log("image extensions");
+        	error_log(print_r(UploadifyField::$image_extensions, 1));
+        	error_log('Image class');
+        	error_log($r->requestVar('imageClass'));
+        	error_log('File class');
+        	error_log($r->requestVar('fileClass'));
+			$class = in_array($ext, UploadifyField::$image_extensions) ? $r->requestVar('imageClass') : $r->requestVar('fileClass');
+
+			error_log("CLAZZ:".$class);
+
+			/*
+			  $image = new Image();
+                $image->Name = $this->Title;
+                $image->Title = $this->Title;
+                $image->Filename = str_replace('../', '', $structure.'/'.$fpid.".jpg");
+                error_log("Setting title to ".$flickrPhoto->Title);
+                $image->Title = $flickrPhoto->Title;
+                //$image->Name = $flickrPhoto->Title;
+                $image->ParentID = $flickrSet->AssetFolderID;
+
+                $image->write();
+
+                 Array\n(\n    [upload] => Array\n        (\n            [name] => IMG_9113.JPG\n            [type] => image/jpeg\n            [tmp_name] => /tmp/phpKmjlva\n            [error] => 0\n            [size] => 631931\n        )\n\n)\n, referer: http://tripodtravel.silverstripe/admin/assets/EditForm/field/Files/upload?SecurityID=5bbf8d7054b5d0a6fe4e3e1a32596b9473a8c66d
+
+
+               */
+
+			//$file = new $class();
+
+			$name = $_FILES['upload']['name'];
+			$mime_type = $_FILES['upload']['type'];
+			$mime_parts = split('/', $mime_type);
+			$core_mime = $mime_parts[0];
+			error_log("Core mime:".$core_mime);
+
+
+			$file = null;
+
+			if ($core_mime == 'image') {
+				error_log("Creating image");
+				$file = new Image();
+			} else {
+				error_log("Creating file");
+				$file = new File();
+			}
+
+
+			error_log("Core file object created:".$file);
+
+			$file->Title = $name;
+			$file->Name = $name;
+
+
+			error_log("name and title set");
+
+
+			//$relativeUploadFolder = str_replace(Director::baseFolder().'/'.ASSETS_DIR, $upload_folder.'/'.$name);
+
+			//error_log("rel upload folder:".$relativeUploadFolder);
+
+			//filename path should be likes of assets/path/to/imagefile.jpg
+
+//			$relative_file_path = $to_path;
+			$relative_file_path = ASSETS_DIR.str_replace($assets_folder_fs, '', $to_path);
+
+			error_log("assets path:".$assets_folder_fs);
+			error_log("Rel assets path:".$relative_file_path);
+
+
+			$file->Filename = $relative_file_path;
+
+
+			error_log("FILE:".$file);
+
+			//$u = new Upload();
+			//error_log("UPLOAD:".$u);
+
+
+			error_log(print_r($_FILES,1));
+
+			// set the parent id
+			if ($folder) {
+				$file->ParentID = $folder->ID;
+			}
+
+			
+			$file->write();
+
+			error_log("FILE ID:".$file->ID);
+			echo $file->ID;
+
+
+
         	exit();
 		} else if(isset($_GET['up'])) {
 			error_log("T3");
@@ -41,7 +166,7 @@ class Html5UploadController extends Controller {
 
         if(file_put_contents($upload_folder.'/'.$headers['UP-FILENAME'], $content)) {
                 error_log("T8");
-                echo 'done';
+                echo '';
         }
         exit();
 }
