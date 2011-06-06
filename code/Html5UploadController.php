a2fe14a9d64e2ac0295c94267168cbea8578a772
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-06-06 13:38:18 +0700

    Html5 uploader: Trace now visible in JS

--- code/Html5UploadController.php
+++ code/Html5UploadController.php
@@ -5,59 +5,60 @@ class Html5UploadController extends Controller {
 
 
 	public function index(SS_HTTPRequest $r) {
-		error_log("UPLOAIFY UPLOADER");
+
+		$trace = ("UPLOAIFY UPLOADER\n    ");
 		if(isset($_FILES["upload"]) && is_uploaded_file($_FILES["upload"]["tmp_name"])) {
-			error_log("FIrst vars ok");
+			$trace .= ("FIrst vars ok\n    ");
 			$upload_folder = urldecode($r->requestVar('uploadFolder'));
 
 			$folder = null;
 
 			if(isset($_REQUEST['FolderID'])) {
-				error_log("FOLDER:".$_REQUEST['FolderID']);
+				$trace .= ("FOLDER:".$_REQUEST['FolderID']."\n    ");
 				if($folder = DataObject::get_by_id("Folder", Convert::raw2sql($_REQUEST['FolderID']))) {
-					error_log("Getting folder");
+					$trace .= ("Getting folder\n    ");
 					$upload_folder = UploadifyField::relative_asset_dir($folder->Filename);
-					error_log("FOLDER FROM PARAM:".$upload_folder);
+					$trace .= ("FOLDER FROM PARAM:".$upload_folder."\n    ");
 				}
 			}
 
-			error_log("UPLOAD FOLDER:".$upload_folder);
+			$trace .= ("UPLOAD FOLDER:".$upload_folder."\n    ");
 
 
  
 			$ext = strtolower(end(explode('.', $_FILES['upload']['name'])));
 
-			error_log("EXTENSION:".$ext);
+			$trace .= ("EXTENSION:".$ext."\n    ");
 
 
 			$mime_type = $_FILES['upload']['type'];
 			$mime_parts = split('/', $mime_type);
 			$core_mime = $mime_parts[0];
-			error_log("Core mime:".$core_mime);
+			$trace .= ("Core mime:".$core_mime."\n    ");
 
 
 			$file = null;
 
 			if ($core_mime == 'image') {
-				error_log("Creating image");
+				$trace .= ("Creating image\n    ");
 				$file = new Image();
 			} else {
-				error_log("Creating file");
+				$trace .= ("Creating file\n    ");
 				$file = new File();
 			}
 
 
 			
 			$u = new Upload();
-			error_log("About to load into $upload_folder");
-			error_log(print_r($_FILES, 1));
+			$trace .= ("About to load into $upload_folder\n    ");
+			$trace .= (print_r($_FILES, 1))."\n    ";
 
 
 			$u->loadIntoFile($_FILES['upload'], $file, $upload_folder);
-			error_log("Loaded into file");
+			$trace .= ("Loaded into file\n    ");
 
 			if ($folder) {
-				error_log("Setting parent id from folder id ".$folder->ID);
+				$trace .= ("Setting parent id from folder id ".$folder->ID."\n    ");
 
 				$file->ParentID = $folder->ID;
 			}
@@ -65,12 +66,15 @@ class Html5UploadController extends Controller {
 
 			$file->write();
 
-			error_log("FILE ID:".$file->ID);
+			$trace .= ("FILE ID:".$file->ID)."\n    ";
 
 
-			$arr = array ('file_id'=>$file->ID);
+			$arr = array ('file_id'=>$file->ID, 'trace' => $trace);
 
 			echo json_encode($arr);
+
+
+			error_log($trace);
 
 		} 
 		else {
@@ -84,100 +88,100 @@ class Html5UploadController extends Controller {
 
 	public function indexOLD(SS_HTTPRequest $r) {
 
-		error_log("ASSETS DIR:".ASSETS_DIR);
-		error_log("BASE FOLDER:".Director::baseFolder());
-		error_log("UPLOAIFY UPLOADER FOR HTML5");
+		$trace .= ("ASSETS DIR:".ASSETS_DIR);
+		$trace .= ("BASE FOLDER:".Director::baseFolder());
+		$trace .= ("UPLOAIFY UPLOADER FOR HTML5");
 
 		$assets_folder_fs = Director::baseFolder().'/'.ASSETS_DIR.'';
 		$upload_folder = $assets_folder_fs.'/flickr';
-error_log('T1');
+$trace .= ('T1');
 
 		if(isset($_REQUEST['FolderID'])) {
-			error_log("**** FOLDER ID SET ON UPLOAD".Convert::raw2sql($_REQUEST['FolderID']));
+			$trace .= ("**** FOLDER ID SET ON UPLOAD".Convert::raw2sql($_REQUEST['FolderID']));
 			if($folder = DataObject::get_by_id("Folder", Convert::raw2sql($_REQUEST['FolderID']))) {
 				$upload_folder = UploadifyField::relative_asset_dir($folder->Filename);
 			}
 		
 		}
 
-error_log('T2');
+$trace .= ('T2');
 
 //folder = $folder = DataObject::get_by_id('Folder', 208);
-		error_log("post get folder");
+		$trace .= ("post get folder");
 
 		//FIXME - hardwired for testing
 		if(isset($folder)) {
-			error_log("Found folder");
+			$trace .= ("Found folder");
 			$upload_folder = UploadifyField::relative_asset_dir($folder->Filename);
 		} else {
 			return 'error';//FIXME better error reporting
 		}
 
-error_log("T3");	
+$trace .= ("T3");	
 	
 
-		error_log("UPLOAD FOLDER:".$upload_folder);
+		$trace .= ("UPLOAD FOLDER:".$upload_folder);
 
 		if(count($_FILES)>0) {
-			error_log("FILES T1");
-			error_log("MOVING FROM:". $_FILES['upload']['tmp_name']);
+			$trace .= ("FILES T1");
+			$trace .= ("MOVING FROM:". $_FILES['upload']['tmp_name']);
 			$to_path = $assets_folder_fs.'/'.$upload_folder.$_FILES['upload']['name'];
-			error_log("TO:".$to_path );
+			$trace .= ("TO:".$to_path );
         	if( move_uploaded_file( $_FILES['upload']['tmp_name'] , $to_path ) ) {
-                error_log("FILES T1a");
+                $trace .= ("FILES T1a");
         	}
-        	error_log("FILES T2");
+        	$trace .= ("FILES T2");
 
 
         	$ext = strtolower(end(explode('.', $_FILES['upload']['name'])));
-        	error_log("EXT:".$ext);
+        	$trace .= ("EXT:".$ext);
 
-        	error_log("image extensions");
-        	error_log(print_r(UploadifyField::$image_extensions, 1));
-        	error_log('Image class');
-        	error_log($r->requestVar('imageClass'));
-        	error_log('File class');
-        	error_log($r->requestVar('fileClass'));
+        	$trace .= ("image extensions");
+        	$trace .= (print_r(UploadifyField::$image_extensions, 1));
+        	$trace .= ('Image class');
+        	$trace .= ($r->requestVar('imageClass'));
+        	$trace .= ('File class');
+        	$trace .= ($r->requestVar('fileClass'));
 			$class = in_array($ext, UploadifyField::$image_extensions) ? $r->requestVar('imageClass') : $r->requestVar('fileClass');
 
-			error_log("CLAZZ:".$class);
+			$trace .= ("CLAZZ:".$class);
 
 			/*
 			  $image = new Image();
                 $image->Name = $this->Title;
                 $image->Title = $this->Title;
                 $image->Filename = str_replace('../', '', $structure.'/'.$fpid.".jpg");
-                error_log("Setting title to ".$flickrPhoto->Title);
+                $trace .= ("Setting title to ".$flickrPhoto->Title);
                 $image->Title = $flickrPhoto->Title;
                 //$image->Name = $flickrPhoto->Title;
                 $image->ParentID = $flickrSet->AssetFolderID;
 
                 $image->write();
 
-                 Array\n(\n    [upload] => Array\n        (\n            [name] => IMG_9113.JPG\n            [type] => image/jpeg\n            [tmp_name] => /tmp/phpKmjlva\n            [error] => 0\n            [size] => 631931\n        )\n\n)\n, referer: http://tripodtravel.silverstripe/admin/assets/EditForm/field/Files/upload?SecurityID=5bbf8d7054b5d0a6fe4e3e1a32596b9473a8c66d
+                 Array\n    (\n        [upload] => Array\n            (\n                [name] => IMG_9113.JPG\n                [type] => image/jpeg\n                [tmp_name] => /tmp/phpKmjlva\n                [error] => 0\n                [size] => 631931\n            )\n    \n    )\n    , referer: http://tripodtravel.silverstripe/admin/assets/EditForm/field/Files/upload?SecurityID=5bbf8d7054b5d0a6fe4e3e1a32596b9473a8c66d
 
 
                */
 
 			//$file = new $class();
 
-			error_log("FILES:");
-			error_log(print_r($FILES,1));
+			$trace .= ("FILES:");
+			$trace .= (print_r($FILES,1));
 
 			$name = $_FILES['upload']['name'];
 			$mime_type = $_FILES['upload']['type'];
 			$mime_parts = split('/', $mime_type);
 			$core_mime = $mime_parts[0];
-			error_log("Core mime:".$core_mime);
+			$trace .= ("Core mime:".$core_mime);
 
 
 			$file = null;
 
 			if ($core_mime == 'image') {
-				error_log("Creating image");
+				$trace .= ("Creating image");
 				$file = new Image();
 			} else {
-				error_log("Creating file");
+				$trace .= ("Creating file");
 				$file = new File();
 			}
 
@@ -188,38 +192,38 @@ error_log("T3");
 /*
 
 
-			error_log("Core file object created:".$file);
+			$trace .= ("Core file object created:".$file);
 
 			$file->Title = $name;
 			$file->Name = $name;
 
 
-			error_log("name and title set");
+			$trace .= ("name and title set");
 
 
 			//$relativeUploadFolder = str_replace(Director::baseFolder().'/'.ASSETS_DIR, $upload_folder.'/'.$name);
 
-			//error_log("rel upload folder:".$relativeUploadFolder);
+			//$trace .= ("rel upload folder:".$relativeUploadFolder);
 
 			//filename path should be likes of assets/path/to/imagefile.jpg
 
 //			$relative_file_path = $to_path;
 			$relative_file_path = ASSETS_DIR.str_replace($assets_folder_fs, '', $to_path);
 
-			error_log("assets path:".$assets_folder_fs);
-			error_log("Rel assets path:".$relative_file_path);
+			$trace .= ("assets path:".$assets_folder_fs);
+			$trace .= ("Rel assets path:".$relative_file_path);
 
 
 			$file->Filename = $relative_file_path;
 
 */
-			error_log("UNPOPULATED FILE:".$file);
+			$trace .= ("UNPOPULATED FILE:".$file);
 
 			//$u = new Upload();
-			//error_log("UPLOAD:".$u);
+			//$trace .= ("UPLOAD:".$u);
 
 
-			error_log(print_r($_FILES,1));
+			$trace .= (print_r($_FILES,1));
 
 			// set the parent id
 			if ($folder) {
@@ -229,16 +233,16 @@ error_log("T3");
 
 			//$upload_folder = $assets_folder_fs.'/'.$upload_folder.$_FILES['upload']['name'];
 			
-			error_log("UPLOAD FOLDER:".$upload_folder);
+			$trace .= ("UPLOAD FOLDER:".$upload_folder);
 
 
-			error_log("UPLOAD - loading file into ********");
-			error_log(print_r($_FILES['upload'],1));
+			$trace .= ("UPLOAD - loading file into ********");
+			$trace .= (print_r($_FILES['upload'],1));
 
 			$u = new Upload();
 			$u->loadIntoFile($_FILES['upload'], $file, $upload_folder);
 			$file->write();
-			error_log("FILE ID:".$file->ID);
+			$trace .= ("FILE ID:".$file->ID);
 
 			$arr = array ('file_id'=>$file->ID);
 
@@ -249,24 +253,24 @@ error_log("T3");
 
         	exit();
 		} else if(isset($_GET['up'])) {
-			error_log("T3");
+			$trace .= ("T3");
         if(isset($_GET['base64'])) {
-        	error_log("T4");
+        	$trace .= ("T4");
                 $content = base64_decode(file_get_contents('php://input'));
         } else {
-        	error_log("T5");
+        	$trace .= ("T5");
                 $content = file_get_contents('php://input');
         }
 
-        error_log("T6");
+        $trace .= ("T6");
 
         $headers = getallheaders();
         $headers = array_change_key_case($headers, CASE_UPPER);
 
-        error_log("T7");
+        $trace .= ("T7");
 
         if(file_put_contents($upload_folder.'/'.$headers['UP-FILENAME'], $content)) {
-                error_log("T8");
+                $trace .= ("T8");
         }
         exit();
 }
